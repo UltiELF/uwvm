0ce930648ed4b5e0c70e1ebb203839e1b552d4eb
MacroModel <liyn_xlfd@163.com>
2024-04-05 22:21:26 +0800

    [path] fix

--- src/utils/install-paths/storge.h
+++ src/utils/install-paths/storge.h
@@ -5,24 +5,8 @@
 
 namespace uwvm::path
 {
-    inline char const*
-#if __has_cpp_attribute(__gnu__::__const__)
-        [[__gnu__::__const__]]
-#endif
-        argv0{};
-    inline ::fast_io::dir_file
-#if __has_cpp_attribute(__gnu__::__const__)
-        [[__gnu__::__const__]]
-#endif
-        module_install_path_df{};
-    inline ::fast_io::u8string
-#if __has_cpp_attribute(__gnu__::__const__)
-        [[__gnu__::__const__]]
-#endif
-        module_path{};
-    inline ::fast_io::u8string
-#if __has_cpp_attribute(__gnu__::__const__)
-        [[__gnu__::__const__]]
-#endif
-        module_install_path{};
+    inline char const* argv0{};
+    inline ::fast_io::dir_file module_install_path_df{};
+    inline ::fast_io::u8string module_path{};
+    inline ::fast_io::u8string module_install_path{};
 }  // namespace uwvm::path
