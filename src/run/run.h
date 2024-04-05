0a2b36b6075d118affbb2c1ead83b01dd151aeb8
MacroModel <liyn_xlfd@163.com>
2024-04-05 15:58:27 +0800

    [run] update

--- src/run/run.h
+++ src/run/run.h
@@ -24,6 +24,29 @@ namespace uwvm
                                 u8"Terminate.\n\n");
             ::fast_io::fast_terminate();
         }
-        ::uwvm::wasm_file_loader = ::fast_io::native_file_loader{::uwvm::wasm_file_name};
+#ifdef __cpp_exceptions
+	try
+	{
+#endif
+            ::uwvm::wasm_file_loader = ::fast_io::native_file_loader{::uwvm::wasm_file_name};
+#ifdef __cpp_exceptions
+	}
+	catch(::fast_io::error e)
+	{
+	     ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+                                u8"\033[97m"
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+                                u8"\033[97m",
+                                e,
+				u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+            ::fast_io::fast_terminate();
+	}
+#endif
     }
 }  // namespace uwvm
