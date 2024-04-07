8553d9fa53a82307cf8f2a31dbe855f1ff67fca2
MacroModel <liyn_xlfd@163.com>
2024-04-07 22:23:43 +0800

    [wasm] update section

--- src/wasm/section.h
+++ src/wasm/section.h
@@ -1,5 +1,4 @@
-#pragma once
-#pragma once
+﻿#pragma once
 #include <cstdint>
 #include <fast_io_core_impl/terminate.h>
 
@@ -7,7 +6,19 @@ namespace uwvm::wasm
 {
     enum section_type : ::std::uint_least8_t
     {
-
+	    custom_sec = 0U,
+	    type_sec,
+	    import_sec,
+	    function_sec,
+	    table_sec,
+	    memory_sec,
+	    global_sec,
+	    export_sec,
+	    start_sec,
+	    element_sec,
+	    code_sec,
+	    data_sec,
+	    data_count_sec
     };
 
 }  // namespace uwvm::wasm
