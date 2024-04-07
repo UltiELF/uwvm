f7978849fe58939de9145699b0114378682df543
MacroModel <liyn_xlfd@163.com>
2024-04-07 22:14:45 +0800

    [wasm] add section

---
+++ src/wasm/section.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+#include <cstdint>
+#include <fast_io_core_impl/terminate.h>
+
+namespace uwvm::wasm
+{
+    enum section_type : ::std::uint_least8_t
+    {
+
+    };
+
+}  // namespace uwvm::wasm
