cade7c58e5543750c75e1fb37a63976ab1cdd04a
MacroModel <liyn_xlfd@163.com>
2024-04-10 20:45:40 +0800

    [detect] update

--- src/run/detect.h
+++ src/run/detect.h
@@ -4,6 +4,8 @@
 #include <io_device.h>
 #include "wasm_file.h"
 
+#include "../wasm/section.h"
+
 namespace uwvm
 {
     inline 
@@ -57,32 +59,8 @@ namespace uwvm
         auto curr{begin};
 
         // min size of wasm file format = 4 + 4
-        if(static_cast<::std::size_t>(end - curr) < 8U) [[unlikely]]
-        {
-            ::fast_io::io::perr(::uwvm::u8err,
-                                u8"\033[0m"
-#ifdef __MSDOS__
-                                u8"\033[37m"
-#else
-                                u8"\033[97m"
-#endif
-                                u8"uwvm: "
-                                u8"\033[31m"
-                                u8"[fatal] "
-                                u8"\033[0m"
-#ifdef __MSDOS__
-                                u8"\033[37m"
-#else
-                                u8"\033[97m"
-#endif
-                                u8"Illegal WASM file format.\n"
-                                u8"\033[0m"
-                                u8"Terminate.\n\n");
-            ::fast_io::fast_terminate();
-        }
-
         // check wasm magic number
-        if(!::uwvm::is_wasm_file_unchecked(curr)) [[unlikely]]
+        if(static_cast<::std::size_t>(end - curr) < 8U || !::uwvm::is_wasm_file_unchecked(curr)) [[unlikely]]
         {
             ::fast_io::io::perr(::uwvm::u8err,
                                 u8"\033[0m"
@@ -113,7 +91,7 @@ namespace uwvm
         // get first section
         curr += 4U;
         
-	// check 1st section
+        // check 1st section
         if(end - curr < 2) [[unlikely]]
         {
             ::fast_io::io::perr(::uwvm::u8err,
@@ -136,6 +114,56 @@ namespace uwvm
                                 u8"\033[0m"
                                 u8"Terminate.\n\n");
             ::fast_io::fast_terminate();
+        }
+
+        // objdump
+        for(;;) 
+        {
+            ::std::uint_fast8_t sec_num{};
+            ::fast_io::freestanding::my_memcpy(__builtin_addressof(sec_num), curr, sizeof(::std::uint_fast8_t));
+            auto sec_type{static_cast<::uwvm::wasm::section_type>(sec_num)};
+            switch(sec_type)
+            {
+                case ::uwvm::wasm::section_type::custom_sec: break;
+                case ::uwvm::wasm::section_type::type_sec: break;
+                case ::uwvm::wasm::section_type::import_sec: break;
+                case ::uwvm::wasm::section_type::function_sec: break;
+                case ::uwvm::wasm::section_type::table_sec: break;
+                case ::uwvm::wasm::section_type::memory_sec: break;
+                case ::uwvm::wasm::section_type::global_sec: break;
+                case ::uwvm::wasm::section_type::export_sec: break;
+                case ::uwvm::wasm::section_type::start_sec: break;
+                case ::uwvm::wasm::section_type::element_sec: break;
+                case ::uwvm::wasm::section_type::code_sec: break;
+                case ::uwvm::wasm::section_type::data_sec: break;
+                case ::uwvm::wasm::section_type::data_count_sec: break;
+                default:
+                {
+                    ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"Unknown WASM section: ",
+                                ::fast_io::mnp::hex0x<true>(sec_num),
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+                    ::fast_io::fast_terminate();
+                }
+            }
+
         }
 
     }
