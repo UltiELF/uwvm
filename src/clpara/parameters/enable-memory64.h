daa21eb071a837aa4b588a430be2a502a56d1161
MacroModel <liyn_xlfd@163.com>
2024-04-11 13:18:27 +0800

    [clpara] update

--- src/clpara/parameters/enable-memory64.h
+++ src/clpara/parameters/enable-memory64.h
@@ -12,14 +12,14 @@ namespace uwvm::parameter
     namespace details
     {
         inline bool enable_memory64_is_exist{};
-        inline constexpr ::fast_io::string_view enable_memory64_alias{"-em64"};
+        inline constexpr ::fast_io::string_view enable_memory64_alias{"-Em64"};
         extern ::uwvm::cmdline::parameter_return_type enable_memory64_callback(::uwvm::cmdline::parameter_parsing_results*,
                                                                     ::fast_io::vector<::uwvm::cmdline::parameter_parsing_results>&) noexcept;
     }  // namespace details
 
     inline constexpr ::uwvm::cmdline::parameter enable_memory64{
         .name{::fast_io::string_view{"--enable-memory64"}},
-        .describe{::fast_io::u8string_view{u8"Enable 64-bit memory indexes for WebAssembly (https://github.com/WebAssembly/memory64). Usage: [--enable-memory64|-em64]"}},
+        .describe{::fast_io::u8string_view{u8"Enable 64-bit memory indexes for WebAssembly (https://github.com/WebAssembly/memory64). Usage: [--enable-memory64|-Em64]"}},
         .alias{::uwvm::cmdline::kns_str_scatter_t{__builtin_addressof(details::enable_memory64_alias), 1}},
         .callback{__builtin_addressof(details::enable_memory64_callback)},
         .is_exist{__builtin_addressof(details::enable_memory64_is_exist)},
