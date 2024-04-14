d9fff87207da0c635d0dfe3066325fba01f3c5a3
MacroModel <liyn_xlfd@163.com>
2024-04-14 19:28:01 +0800

    [section] update

---
+++ src/run/wasm/section/function/function_normal.h
@@ -0,0 +1,220 @@
+#pragma once
+
+namespace uwvm
+{
+    inline void scan_function_section(::std::byte const* begin, ::std::byte const* end) noexcept
+    {
+#ifdef UWVM_TIMER
+        ::fast_io::timer scan_type_section_timer{u8"uwvm: [timer] scan function section"};
+#endif
+        // alias def
+        using char8_t_may_alias_ptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+            [[__gnu__::__may_alias__]]
+#endif
+            = char8_t*;
+        using char8_t_const_may_alias_ptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+            [[__gnu__::__may_alias__]]
+#endif
+            = char8_t const*;
+
+        // check is exist
+        // always check
+        if(::uwvm::global_function_section.sec_begin) [[unlikely]]
+        {
+            ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"Duplicate WASM Section: Function."
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+            ::fast_io::fast_terminate();
+        }
+        ::uwvm::global_function_section.sec_begin = begin;
+        ::uwvm::global_function_section.sec_end = end;
+
+        // curr
+        auto curr{begin};
+
+        // get function size
+        ::std::size_t function_count{};
+        auto [next, err]{::fast_io::parse_by_scan(reinterpret_cast<char8_t_const_may_alias_ptr>(curr),
+                                                  reinterpret_cast<char8_t_const_may_alias_ptr>(end),
+                                                  ::fast_io::mnp::leb128_get(function_count))};
+        switch(err)
+        {
+            case ::fast_io::parse_code::ok: break;
+            default:
+                [[unlikely]]
+                {
+                    ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"Invalid function length."
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+                    ::fast_io::fast_terminate();
+                }
+        }
+
+        // check 64-bit indexes
+        ::uwvm::check_index(function_count);
+
+        ::uwvm::global_function_section.function_count = function_count;
+        ::uwvm::global_function_section.types.reserve(function_count);
+
+        // jump to functype
+        curr = reinterpret_cast<::std::byte const*>(next);
+
+        auto const type_table_basic_index{::uwvm::global_type_section.types.cbegin()};
+        auto const type_table_size{::uwvm::global_type_section.types.size()};
+
+        ::std::size_t function_counter{};
+        for(; curr < end;)
+        {
+            if(++function_counter > function_count) [[unlikely]]
+            {
+                ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"The number of functions resolved does not match the actual number."
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+                ::fast_io::fast_terminate();
+            }
+
+            ::std::size_t type_index_len{};
+            auto [next_type_index, err_type_index]{::fast_io::parse_by_scan(reinterpret_cast<char8_t_const_may_alias_ptr>(curr),
+                                                                            reinterpret_cast<char8_t_const_may_alias_ptr>(end),
+                                                                            ::fast_io::mnp::leb128_get(type_index_len))};
+            switch(err_type_index)
+            {
+                case ::fast_io::parse_code::ok: break;
+                default:
+                    [[unlikely]]
+                    {
+                        ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"Invalid index length."
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+                        ::fast_io::fast_terminate();
+                    }
+            }
+
+            // check
+            if(type_index_len >= type_table_size) [[unlikely]]
+            {
+                ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"Invalid type index."
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+                ::fast_io::fast_terminate();
+            }
+
+            ::uwvm::global_function_section.types.emplace_back_unchecked(type_table_basic_index + type_index_len);
+            // jump to para1
+            curr = reinterpret_cast<::std::byte const*>(next_type_index);
+        }
+        // check func counter
+        if(function_counter != function_count) [[unlikely]]
+        {
+            ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+                                u8"\033[31m"
+                                u8"[fatal] "
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"The number of functions resolved does not match the actual number."
+                                u8"\n"
+                                u8"\033[0m"
+                                u8"Terminate.\n\n");
+            ::fast_io::fast_terminate();
+        }
+    }
+
+}  // namespace uwvm
