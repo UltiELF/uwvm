ac3e95200f819b876d08a9cf0111f9ec6e64f2ca
MacroModel <liyn_xlfd@163.com>
2024-04-15 11:42:11 +0800

    [clpara] reserve help

--- src/clpara/parameters/help.cpp
+++ src/clpara/parameters/help.cpp
@@ -1,52 +1,51 @@
 ﻿#include "help.h"
 #include "../parameters.h"
 
-namespace uwvm::parameter::details
+#if __has_cpp_attribute(__gnu__::__cold__)
+[[__gnu__::__cold__]]
+#endif
+::uwvm::cmdline::parameter_return_type(::uwvm::parameter::details::help_callback)(::uwvm::cmdline::parameter_parsing_results*,
+                                                                                  ::fast_io::vector<::uwvm::cmdline::parameter_parsing_results>&) noexcept
 {
-    struct help_printer
-    {
-    };
-
-    template <typename s>
-    inline constexpr void print_define(::fast_io::io_reserve_type_t<char8_t, help_printer>, s&& stm, help_printer) noexcept
-    {
-        ::fast_io::operations::print_freestanding<false>(::std::forward<s>(stm),
-                                                         u8"\033[0m"
+    ::fast_io::basic_obuf<decltype(::uwvm::u8err)> buf_u8out{::uwvm::u8err};
+    ::fast_io::io::perr(buf_u8out,
+                        u8"\033[0m"
 #ifdef __MSDOS__
-                                                         u8"\033[37m"
+                        u8"\033[37m"
 #else
-                                                         u8"\033[97m"
+                        u8"\033[97m"
 #endif
-                                                         u8"Arguments:\n");
-        for(auto const& p: ::uwvm::parameters)
+                        u8"Arguments:\n");
+    for(auto const& p: ::uwvm::parameters)
+    {
+        ::fast_io::io::perr(buf_u8out,
+                            u8"\t"
+                            u8"\033[32m",
+                            ::fast_io::mnp::code_cvt(p->name));
+        if(p->alias.len != 0)
         {
-            ::fast_io::operations::print_freestanding<false>(::std::forward<s>(stm), u8"\t" u8"\033[32m", ::fast_io::mnp::code_cvt(p->name));
-            if(p->alias.len != 0)
+            ::fast_io::io::perr(buf_u8out,
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8", ");
+            for(::std::size_t i{}; i < p->alias.len - 1; i++)
             {
-                ::fast_io::operations::print_freestanding<false>(::std::forward<s>(stm),
+                ::fast_io::io::perr(buf_u8out,
+                                    u8"\033[32m",
+                                    ::fast_io::mnp::code_cvt(p->alias.base[i]),
 #ifdef __MSDOS__
-                                                                 u8"\033[37m"
+                                    u8"\033[37m"
 #else
-                                                                 u8"\033[97m"
+                                    u8"\033[97m"
 #endif
-                                                                 u8", ");
-                for(::std::size_t i{}; i < p->alias.len - 1; i++)
-                {
-                    ::fast_io::operations::print_freestanding<false>(::std::forward<s>(stm),
-                                                                     u8"\033[32m",
-                                                                     ::fast_io::mnp::code_cvt(p->alias.base[i]),
-#ifdef __MSDOS__
-                                                                     u8"\033[37m"
-#else
-                                                                     u8"\033[97m"
-#endif
-                                                                     u8", ");
-                }
-                ::fast_io::operations::print_freestanding<false>(::std::forward<s>(stm),
-                                                                 u8"\033[32m",
-                                                                 ::fast_io::mnp::code_cvt(p->alias.base[p->alias.len - 1]));
+                                    u8", ");
             }
-            ::fast_io::operations::print_freestanding<true>(::std::forward<s>(stm),
+            ::fast_io::io::perr(buf_u8out, u8"\033[32m", ::fast_io::mnp::code_cvt(p->alias.base[p->alias.len - 1]));
+        }
+        ::fast_io::io::perrln(buf_u8out,
                               u8"\033[37m"
                               u8" --- "
 #ifdef __MSDOS__
@@ -56,18 +55,10 @@ namespace uwvm::parameter::details
 #endif
                               ,
                               p->describe);
-        }
-        ::fast_io::operations::print_freestanding<false>(::std::forward<s>(stm), u8"\033[0m" u8"\n");
     }
-}  // namespace uwvm::parameter::details
+    ::fast_io::io::perr(buf_u8out,
+                        u8"\033[0m"
+                        u8"\n");
 
-#if __has_cpp_attribute(__gnu__::__cold__)
-[[__gnu__::__cold__]]
-#endif
-::uwvm::cmdline::parameter_return_type(::uwvm::parameter::details::help_callback)(::uwvm::cmdline::parameter_parsing_results*,
-                                                                                  ::fast_io::vector<::uwvm::cmdline::parameter_parsing_results>&) noexcept
-{
-    ::fast_io::basic_obuf<decltype(::uwvm::u8err)> u8errbuf{::uwvm::u8err};
-    ::fast_io::io::perr(u8errbuf, ::uwvm::parameter::details::help_printer{});
     return ::uwvm::cmdline::parameter_return_type::return_imme;
 }
