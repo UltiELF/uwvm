031a0acc5a29e35ebc4e8f7c3255d1720a792935
MacroModel <liyn_xlfd@163.com>
2024-04-16 19:59:27 +0800

    [objdump] update,timer

--- src/run/objdump.h
+++ src/run/objdump.h
@@ -7,10 +7,10 @@ namespace uwvm
 {
     inline void u8objdump() noexcept
     {
-        ::fast_io::basic_obuf<decltype(::uwvm::u8out)> u8outbuf{::uwvm::u8out};
 #ifdef UWVM_TIMER
         ::fast_io::timer objdump_timer{u8"uwvm: [timer] objdump"};
 #endif
+        ::fast_io::basic_obuf<decltype(::uwvm::u8out)> u8outbuf{::uwvm::u8out};
         ::fast_io::print(u8outbuf, objdump(::uwvm::global_wasm_module));
     }
 }  // namespace uwvm
