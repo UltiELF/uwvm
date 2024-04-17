fed520182a692756508f2cc6c59736c18f454045
MacroModel <liyn_xlfd@163.com>
2024-04-17 12:54:34 +0800

    [objdump] update

--- src/run/objdump.h
+++ src/run/objdump.h
@@ -11,6 +11,6 @@ namespace uwvm
         ::fast_io::timer objdump_timer{u8"uwvm: [timer] objdump"};
 #endif
         ::fast_io::basic_obuf<decltype(::uwvm::u8out)> u8outbuf{::uwvm::u8out};
-        ::fast_io::print(u8outbuf, objdump(::uwvm::global_wasm_module));
+        ::fast_io::io::print(u8outbuf, objdump(::uwvm::global_wasm_module));
     }
 }  // namespace uwvm
