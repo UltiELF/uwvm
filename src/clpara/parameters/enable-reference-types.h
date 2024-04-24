f3e83397378c94a4f08eacc1928494c9be3486f5
MacroModel <liyn_xlfd@163.com>
2024-04-24 12:27:46 +0800

    [clpara] update

--- src/clpara/parameters/enable-reference-types.h
+++ src/clpara/parameters/enable-reference-types.h
@@ -17,7 +17,7 @@ namespace uwvm::parameter
     inline constexpr ::uwvm::cmdline::parameter enable_reference_types{
         .name{::fast_io::string_view{"--enable-reference-types"}},
         .describe{::fast_io::u8string_view{
-            u8"Enable type reflection for WebAssembly (https://gitee.com/link?target=https%3A%2F%2Fgithub.com%2FWebAssembly%2Fjs-types%2Fblob%2Fmain%2Fproposals%2Fjs-types%2FOverview.md). Usage: [--enable-reference-types|-Eref]"}},
+            u8"Enable type reflection for WebAssembly (https://github.com/WebAssembly/reference-types/blob/master/proposals/reference-types/Overview.md). Usage: [--enable-reference-types|-Eref]"}},
         .alias{::uwvm::cmdline::kns_str_scatter_t{__builtin_addressof(details::enable_reference_types_alias), 1}},
         .is_exist{__builtin_addressof(::uwvm::features::enable_reference_types)},
     };
