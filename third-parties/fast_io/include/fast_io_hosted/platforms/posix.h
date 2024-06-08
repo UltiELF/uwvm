0f3406f4ad3541901c5efcf4c5458b57e8860926
MacroModel <liyn_xlfd@163.com>
2024-06-08 21:54:21 +0800

    [fast_io] fix dos compile error

--- third-parties/fast_io/include/fast_io_hosted/platforms/posix.h
+++ third-parties/fast_io/include/fast_io_hosted/platforms/posix.h
@@ -1324,6 +1324,8 @@ public:
 		int a2[2]{-1, -1};
 #if (defined(_WIN32) && !defined(__WINE__) && !defined(__BIONIC__)) && !defined(__CYGWIN__)
 		if (noexcept_call(_pipe, a2, 131072u, _O_BINARY) == -1)
+#elif defined(__MSDOS__)
+		if (noexcept_call(::pipe, a2) == -1)
 #else
 		if (noexcept_call(::pipe2, a2, O_CLOEXEC) == -1)
 #endif
