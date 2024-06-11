16c94f06aae0e7ecc093c8d7a5c636a247a1def6
MacroModel <liyn_xlfd@163.com>
2024-06-12 00:10:46 +0800

    [wasm] add custom scanner

---
+++ src/wasm/custom.h
@@ -0,0 +1,2 @@
+#pragma once
+#include "custom/name.h"
---
+++ src/wasm/custom/name.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../section/custom.h"
+
+namespace uwvm::wasm::custom
+{
+    inline constexpr bool scan_name_custom_section(::uwvm::wasm::section::custom_section const& cs) noexcept 
+    {
+
+    }
+
+}
