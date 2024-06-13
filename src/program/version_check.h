604f9babe4325fbec060bcc662032b228ed88d1a
MacroModel <liyn_xlfd@163.com>
2024-06-13 16:11:38 +0800

    [libcxx] support libcxx

--- src/program/version_check.h
+++ src/program/version_check.h
@@ -9,7 +9,7 @@
 // __cpp_lib_three_way_comparison
 #if 0
     #ifndef __cpp_lib_three_way_comparison
-    // libcxx not supported yet: https://github.com/llvm/llvm-project/blob/main/libcxx%2Finclude%2Fversion
+        // libcxx not supported yet: https://github.com/llvm/llvm-project/blob/main/libcxx%2Finclude%2Fversion
         #error "unsupported c++ std library"
     #endif
 #endif
