0ccaa93c2d0dd65e4f89b5f7228e38a962e7c67f
MacroModel <liyn_xlfd@163.com>
2024-07-14 17:24:01 +0800

    [wasm] fix

--- src/vm/interpreter/wasi/wasi.h
+++ src/vm/interpreter/wasi/wasi.h
@@ -1,12 +1,112 @@
 #pragma once
 #include <fast_io.h>
 #include "abi.h"
+#include "../../../run/wasm_file.h"
+#include "../../../clpara/parsing_result.h"
 
 namespace uwvm::vm::interpreter::wasi
 {
-    ::std::int_least32_t args_get(::std::int_least32_t arg0, ::std::int_least32_t arg1) noexcept { return {}; }
+    ::std::int_least32_t args_get(::std::int_least32_t arg0, ::std::int_least32_t arg1) noexcept
+    {
+        auto& memory{::uwvm::vm::interpreter::memories.front()};
+        auto const memory_begin{memory.memory_begin};
+        auto const memory_length{memory.memory_length};
+        auto const memory_end{memory_begin + memory_length};
 
-    ::std::int_least32_t args_sizes_get(::std::int_least32_t arg0, ::std::int_least32_t arg1) noexcept { return {}; }
+        auto argv_begin{memory_begin + static_cast<::std::size_t>(arg0)};
+
+        auto const pr_cend{::uwvm::parsing_result.cend()};
+
+        if(argv_begin + sizeof(::std::uint_least32_t) * static_cast<::std::size_t>(pr_cend - (::uwvm::parsing_result.cbegin() + ::uwvm::wasm_file_ppos)) >=
+           memory_end) [[unlikely]]
+        {
+            return static_cast<::std::int_least32_t>(::uwvm::vm::interpreter::wasi::errno_t::efault);
+        }
+
+        using uint_least32_t_may_alias_ptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+            [[__gnu__::__may_alias__]]
+#endif
+            = ::std::uint_least32_t*;
+
+        auto argv_i32p{reinterpret_cast<uint_least32_t_may_alias_ptr>(argv_begin)};
+
+        using char8_t_may_alias_ptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+            [[__gnu__::__may_alias__]]
+#endif
+            = char8_t*;
+
+        auto argv_buf_begin{reinterpret_cast<char8_t_may_alias_ptr>(memory_begin + static_cast<::std::size_t>(arg1))};
+
+        for(auto i{::uwvm::parsing_result.cbegin() + ::uwvm::wasm_file_ppos}; i < pr_cend; ++i)
+        {
+            auto const argv_buf_offset_le32{
+                ::fast_io::little_endian(static_cast<::std::uint_least32_t>(argv_buf_begin - reinterpret_cast<char8_t_may_alias_ptr>(memory_begin)))};
+            ::fast_io::freestanding::my_memcpy(argv_i32p, __builtin_addressof(argv_buf_offset_le32), sizeof(argv_buf_offset_le32));
+            auto const length{i->str.size()};
+
+            if(reinterpret_cast<::std::byte*>(argv_buf_begin) + length >= memory_end) [[unlikely]]
+            {
+                return static_cast<::std::int_least32_t>(::uwvm::vm::interpreter::wasi::errno_t::efault);
+            }
+
+            ::fast_io::freestanding::my_memcpy(argv_buf_begin, i->str.data(), length);
+
+            ++argv_i32p;
+            argv_buf_begin += length;
+            *argv_buf_begin++ = u8'\0';
+        }
+
+        return static_cast<::std::int_least32_t>(::uwvm::vm::interpreter::wasi::errno_t::esuccess);
+    }
+
+    ::std::int_least32_t args_sizes_get(::std::int_least32_t arg0, ::std::int_least32_t arg1) noexcept
+    {
+        auto& memory{::uwvm::vm::interpreter::memories.front()};
+        auto const memory_begin{memory.memory_begin};
+        auto const memory_length{memory.memory_length};
+        auto const memory_end{memory_begin + memory_length};
+        auto const pr_cend{::uwvm::parsing_result.cend()};
+
+        // argc begin
+        auto const argc_begin{memory_begin + static_cast<::std::size_t>(arg0)};
+        if(argc_begin + sizeof(::uwvm::vm::interpreter::wasi::wasi_size_t) >= memory_end) [[unlikely]]
+        {
+            return static_cast<::std::int_least32_t>(::uwvm::vm::interpreter::wasi::errno_t::efault);
+        }
+
+        using wasi_size_t_may_alias_ptr
+#if __has_cpp_attribute(__gnu__::__may_alias__)
+            [[__gnu__::__may_alias__]]
+#endif
+            = ::uwvm::vm::interpreter::wasi::wasi_size_t*;
+
+        auto argv_i32p{reinterpret_cast<wasi_size_t_may_alias_ptr>(argc_begin)};
+
+        auto const argv_wasm{static_cast<::uwvm::vm::interpreter::wasi::wasi_size_t>(pr_cend - (::uwvm::parsing_result.cbegin() + ::uwvm::wasm_file_ppos))};
+
+        auto const argv_wasm_le32{::fast_io::little_endian(static_cast<::std::uint_least32_t>(argv_wasm))};
+
+        ::fast_io::freestanding::my_memcpy(argv_i32p, __builtin_addressof(argv_wasm_le32), sizeof(::std::uint_least32_t));
+
+        // buf len
+        auto const buf_len_begin{memory_begin + static_cast<::std::size_t>(arg1)};
+        if(buf_len_begin + sizeof(::uwvm::vm::interpreter::wasi::wasi_size_t) >= memory_end) [[unlikely]]
+        {
+            return static_cast<::std::int_least32_t>(::uwvm::vm::interpreter::wasi::errno_t::efault);
+        }
+
+        ::std::uint_least32_t buf_len{};
+
+        for(auto i{::uwvm::parsing_result.cbegin() + ::uwvm::wasm_file_ppos}; i < pr_cend; ++i) { buf_len += i->str.size() + 1; }
+
+        auto const buf_len_le32{::fast_io::little_endian(static_cast<::std::uint_least32_t>(buf_len))};
+
+        ::fast_io::freestanding::my_memcpy(buf_len_begin, __builtin_addressof(buf_len), sizeof(::std::uint_least32_t));
+
+        return static_cast<::std::int_least32_t>(::uwvm::vm::interpreter::wasi::errno_t::esuccess);
+    }
 
     ::std::int_least32_t environ_get(::std::int_least32_t arg0, ::std::int_least32_t arg1) noexcept { return {}; }
 
