202a3dafc68454a92cb56781ff36a68455588a25
MacroModel <liyn_xlfd@163.com>
2024-08-16 15:30:51 +0800

    [int] update bt

--- src/vm/interpreter/int_bt.cpp
+++ src/vm/interpreter/int_bt.cpp
@@ -6,10 +6,38 @@
 void uwvm::vm::interpreter::int_bt() noexcept
 {
     {
-        ::fast_io::io::perr(::uwvm::u8err, u8"\ninterpreter backtrace:\n");
+        ::fast_io::io::perr(::uwvm::u8err,
+                                u8"\033[0m"
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"uwvm: "
+#ifdef __MSDOS__
+                                u8"\033[37m"
+#else
+                                u8"\033[97m"
+#endif
+                                u8"[info] "
+                                u8"Interpreter backtrace:\n"
+                                u8"\033[0m");
 
         ::std::size_t c{};
-        for(auto const& i: ::uwvm::vm::interpreter::int_call_stack.get_container()) { ::fast_io::io::perrln(::uwvm::u8err, u8"(", c++, u8") ", i); }
-        ::fast_io::io::perrln(::uwvm::u8err);
+        for(auto const& i: ::uwvm::vm::interpreter::int_call_stack.get_container())
+        {
+            ::fast_io::io::perrln(::uwvm::u8err,
+                                  u8"\033[35m" u8"(",
+                                  c++,
+                                  u8") "
+#ifdef __MSDOS__
+                                  u8"\033[37m"
+#else
+                                  u8"\033[97m"
+#endif
+                                  ,
+                                  i);
+        }
+        ::fast_io::io::perr(::uwvm::u8err, u8"\n" u8"\033[0m");
     }
 }
