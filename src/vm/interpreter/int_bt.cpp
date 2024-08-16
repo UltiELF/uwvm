3a0257284f251250ae0876cff34ddbda3c1ddab1
MacroModel <liyn_xlfd@163.com>
2024-08-16 15:16:31 +0800

    [int] update

--- src/vm/interpreter/int_bt.cpp
+++ src/vm/interpreter/int_bt.cpp
@@ -6,9 +6,10 @@
 void uwvm::vm::interpreter::int_bt() noexcept
 {
     {
-        ::fast_io::io::perr(::uwvm::u8err, u8"interpreter backtrace:\n");
+        ::fast_io::io::perr(::uwvm::u8err, u8"\ninterpreter backtrace:\n");
 
         ::std::size_t c{};
-        for(auto const& i: ::uwvm::vm::interpreter::int_call_stack.get_container()) { ::fast_io::io::perrln(::uwvm::u8err, u8"(", c++, u8")", i); }
+        for(auto const& i: ::uwvm::vm::interpreter::int_call_stack.get_container()) { ::fast_io::io::perrln(::uwvm::u8err, u8"(", c++, u8") ", i); }
+        ::fast_io::io::perrln(::uwvm::u8err);
     }
 }
